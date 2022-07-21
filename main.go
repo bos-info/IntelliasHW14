e688c16d49a8539e3159ee0a8f9403a4bbab420e
bos <a@bos-info.com>
2022-07-21 17:49:18 +0300

    використовуємо модифікований слайс

--- main.go
+++ main.go
@@ -15,30 +15,33 @@ import (
 // “slice 2: 16”
 
 func main() {
-	// Розкоментуй мене)
 	var wg sync.WaitGroup
 	n := [][]int{
 		{2, 6, 9, 24},
 		{7, 3, 94, 3, 0},
 		{4, 2, 8, 35},
 	}
-	//use channels only for test  purpose
-	ch := make(chan int, len(n))
 	wg.Add(len(n))
 	for i := 0; i < len(n); i++ {
+		//оскільки вимагається передача ЛИШЕ слайсу інтів до функції sum
+		//то за допомогою замикання ми створюємо модифікований слайс, де перше(0) значення завжди номер слайсу
+		//якщо додалиб рядок створення модифікованого слайсу всередину функції отримали б гонку
+		//оскільки апенд дорога операція створюємо слайс заздалегідь відомого розміру
+		modSlice := make([]int, 1, len(n[i]))
+		modSlice[0] = i + 1
 		go func(incr int) {
-			ch <- incr + 1
+			modSlice = append(modSlice, n[incr]...)
 			defer wg.Done()
-			sum(n[incr], ch)
+			sum(modSlice)
 		}(i)
 	}
 	wg.Wait()
 }
 
-func sum(sl []int, ch chan int) {
+func sum(sl []int) {
 	summa := 0
-	counter := <-ch
-	for _, v := range sl {
+	counter := sl[0]
+	for _, v := range sl[1:] {
 		summa += v
 	}
 	fmt.Printf("slice %d: %d\n", counter, summa)
